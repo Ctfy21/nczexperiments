5cc1eed8b07da437261441630d73243427dadea4
Ctfy <skileev123@gmail.com>
2024-02-05 13:04:26 +0300

    upgrade currentValue and cuerrentValues cubit

--- lib/cubit/current_value/current_value_cubit.dart
+++ lib/cubit/current_value/current_value_cubit.dart
@@ -3,21 +3,13 @@ import 'dart:async';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nczexperiments/cubit/current_value/current_value_repository.dart';
 import 'package:nczexperiments/cubit/current_value/current_value_state.dart';
-import 'package:nczexperiments/cubit/experiments/experiments_cubit.dart';
-import 'package:nczexperiments/cubit/experiments/experiments_state.dart';
+import 'package:nczexperiments/models/current_value.dart';
+import 'package:nczexperiments/models/experiment.dart';
 
 class CurrentValueCubit extends Cubit<CurrentValueState>{
   final CurrentValueRepository currentValueRepository;
-  final ExperimentsCubit experimentsCubit;
-  late final StreamSubscription experimentsSubscription;
 
-  CurrentValueCubit({required this.currentValueRepository, required this.experimentsCubit}) : super(const CurrentValueInitial()){
-    experimentsSubscription = experimentsCubit.stream.listen((ExperimentsState state) {
-      if(state is ExperimentsSuccess){
-        //Продолжение здесь!!!!!!
-      }
-    });
-  }
+  CurrentValueCubit(this.currentValueRepository) : super(const CurrentValueInitial());
 
   Future<void> getCurrentValue(int id) async{
     try{
@@ -30,10 +22,29 @@ class CurrentValueCubit extends Cubit<CurrentValueState>{
     }
   }
 
-  @override
-  Future<void> close(){
-    experimentsSubscription.cancel();
-    return super.close();
+  Future<void> getCurrentValuesFromExperiment(Experiment experiment) async{
+    emit(const CurrentValuesInitial());
+    try{
+      List<CurrentValue> currentValues = [];
+      emit(const CurrentValuesLoading());
+      for(var id in experiment.currentValues){
+        currentValues.add(await currentValueRepository.fetchCurrentValue("https://protiraki.beget.app/api/currentvaluesdetail/${id.toString()}"));
+      }
+      emit(CurrentValuesSuccess(currentValues));
+    }
+    catch(e){
+      emit(CurrentValueError("Проблемы с ${e.toString()}"));
+    }
+  }
+
+  void setCurrentValue(CurrentValue currentValue){  
+    emit(state.copyWith(newCurrentValue: currentValue));
+  }
+
+  void addCurrentValue(CurrentValue newCurrentValue) {
+    final currentValues = List<CurrentValue>.from(state.currentValues ?? []);
+    currentValues.add(newCurrentValue);
+    emit(state.copyWith(newCurrentValues: currentValues));
   }
 
 }
--- lib/cubit/current_value/current_value_state.dart
+++ lib/cubit/current_value/current_value_state.dart
@@ -1,8 +1,20 @@
 import 'package:nczexperiments/models/current_value.dart';
 
-abstract class CurrentValueState {
-  const CurrentValueState();
+class CurrentValueState {
+  final CurrentValue? currentValue;
+  final List<CurrentValue>? currentValues;
+
+  const CurrentValueState({this.currentValue, this.currentValues});
+
+    CurrentValueState copyWith({CurrentValue? newCurrentValue, List<CurrentValue>? newCurrentValues}) {
+    return CurrentValueState(
+      currentValue: newCurrentValue ?? currentValue,
+      currentValues: newCurrentValues ?? currentValues,
+    );
+  } 
 }
+
+// CurrentValueManager
 
 class CurrentValueInitial extends CurrentValueState{
   const CurrentValueInitial();
@@ -13,6 +25,7 @@ class CurrentValueLoading extends CurrentValueState{
 }
 
 class CurrentValueSuccess extends CurrentValueState{
+  // ignore: annotate_overrides, overridden_fields
   final CurrentValue currentValue;
   const CurrentValueSuccess(this.currentValue);
 
@@ -36,6 +49,49 @@ class CurrentValueError extends CurrentValueState{
     if (identical(this, other)) return true;
 
     return other is CurrentValueError && other.message == message;
+  }
+
+  @override
+  int get hashCode => message.hashCode;
+
+}
+
+// CurrentValuesStateManager
+
+class CurrentValuesInitial extends CurrentValueState{
+  const CurrentValuesInitial();
+}
+
+class CurrentValuesLoading extends CurrentValueState{
+  const CurrentValuesLoading();
+}
+
+
+class CurrentValuesSuccess extends CurrentValueState{
+  // ignore: annotate_overrides, overridden_fields
+  final List<CurrentValue> currentValues;
+  const CurrentValuesSuccess(this.currentValues);
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is CurrentValuesSuccess && other.currentValues == currentValues;
+  }
+
+  @override
+  int get hashCode => currentValues.hashCode;
+}
+
+class CurrentValuesError extends CurrentValueState{
+  final String message;
+  const CurrentValuesError(this.message);
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is CurrentValuesError && other.message == message;
   }
 
   @override
